ce5d9c50a9cae5eccb318fc20c62edc35457bee5
Edgar Canro <edgar.canro@gmail.com>
2021-04-11 21:02:49 +0100

    Day 68-69: End athentication and modify Blog to accepts authentication

--- Day_68/main.py
+++ Day_68/main.py
@@ -10,13 +10,22 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 ##CREATE TABLE IN DB
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
-#Line below only required once, when creating DB. 
+
+# Line below only required once, when creating DB.
 # db.create_all()
 
 
@@ -28,30 +37,60 @@ def home():
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
+        hash_and_salted_password = generate_password_hash(
+            request.form.get('password'),
+            method='pbkdf2:sha256',
+            salt_length=8
+        )
         new_user = User(
             email=request.form.get('email'),
             name=request.form.get('name'),
-            password=request.form.get('password')
+            password=hash_and_salted_password
         )
         db.session.add(new_user)
         db.session.commit()
+
+        # Log in and authenticate user after adding details to database.
+        login_user(new_user)
+
         return redirect(url_for('secrets'))
     return render_template("register.html")
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form.get('email')
+        password = request.form.get('password')
+
+        # find user by email
+        user = User.query.filter_by(email=email).first()
+
+        # check user
+        if not user:
+            flash('That email does not exist, please try again.')
+            return redirect(url_for('login'))
+        # check password
+        elif not check_password_hash(user.password, password):
+            flash('Passsword incorrect, please try again')
+            return redirect(url_for('login'))
+        else:
+            login_user(user)
+            return redirect(url_for('secrets'))
+
     return render_template("login.html")
 
 
 @app.route('/secrets')
 def secrets():
-    return render_template("secrets.html")
+    print(current_user.name)
+    return render_template("secrets.html", name=current_user.name)
 
 
 @app.route('/logout')
 def logout():
-    pass
+    logout_user()
+    return redirect(url_for('home'))
 
 
 @app.route('/download')
